f639eb548d7c7bb6279860f0e0e2660bc747c970
erlinaputrirahmadhani05 <erlinarahmadhani05@gmail.com>
2024-07-16 13:47:55 +0700

    vsga

---
+++ app/src/main/java/dev/erlina/aplikasimobile/MainActivity.java
@@ -0,0 +1,28 @@
+package dev.erlina.aplikasimobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText TextNama;
+    TextView Hasil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextNama=(EditText)findViewById(R.id.TxtName);
+        Hasil=(TextView)findViewById(R.id.Lbl_Nama);
+    }
+
+    public void TampilNama(View v){
+        Hasil.setText("Nama Anda : "+TextNama.getText());
+    }
+}
